23541c0ac1a00d9c13174606597f388a769b75d9
Esteban Rios <esteban.rios@viseo.com>
2018-07-10 01:02:58 +0200

    refacto

--- index.js
+++ index.js
@@ -1,110 +1,230 @@
+const fs = require('fs');
+
 /**
+ * Constructor
+ * @param {object} params
+ * @exemple params = {
+ *  modulesFilePath: '/path_to_file_modules.isml',
+ *  editorTarget: 'vscode'  // ['vscode'|'sublimetext'],
+ *  paths: {
+ *    vscode: {
+ *      outputpath: 'PERSONALIZED_PATH'
+ *    },
+ *    sublimetext: {
+ *      outputpath: 'PERSONALIZED_PATH'
+ *    }
+ *  }
+ * }
  * 
- * @param {string} str 
- * @param {string} (vscode|sublimetext) editorTarget 
+ * @returns object
  */
-/**
- * generate snippets from data from components.isml
- *
- * @param  {String} html
- * @return {object} instance of GeneratorSnippets
- */
-function GeneratorSnippets(str, editorTarget) {
-  this.stringIn = str;
-  this.editorTarget = editorTarget || 'sublimetext';
+function GeneratorSnippets(params) {
+  let paths;
+
+  if (!params || params && !params.modulesFilePath) {
+    console.error('params & params.modulesFilePath is mandatory!');
+    return false;
+  }
+
+  this.stringIn = this.getData(params.modulesFilePath);
+  this.editorTarget = params.editorTarget || 'vscode';
   this.modules = [];
   this.templatesSnippets = {};
+  this.writters = {
+    vscode: () => { console.log('template writter empty'); }
+  };
 
+  if (!params.paths) {
+    paths = {};
+    paths.vscode = {
+      outputpath: process.cwd() + '/snippets/vscode/isml.json'
+    };
+    paths.sublimetext = {
+      outputpath: process.cwd() + '/snippets/sublimetext/'
+    };
+  } else {
+    paths = params.paths;
+  }
+
+  this.outputpath = paths[this.editorTarget] && paths[this.editorTarget].outputpath ?
+    paths[this.editorTarget].outputpath
+    :
+    paths.vscode.outputpath;
+
+  this.prepareWritter();
   this.prepareTemplatesSnippets();
   this.prepareData();
-};
+
+  return this;
+}
 
 GeneratorSnippets.prototype.prepareTemplatesSnippets = function () {
+
   this.templatesSnippets.vscode = function name() {
     return function name(module) {
-      var nameModule = module.name;
-      var paramsMod = module.params;
-      var params = [];
+      let paramsToTpl;
+      let tpl;
+      let nameModule = module.name;
+      let paramsMod = module.params;
+      let params = [];
+      let prefix = nameModule;
 
-      for (var y = 0; y < paramsMod.length; y++) {
-        var count = y + 1;
-        params.push(`"${paramsMod[y]}=` + `\\\"$${count}\\\""`)
-      }
+      params = paramsMod.map((mod, index) => {
+        let count = index + 1;
+        return `"${mod}=` + `\\\"$${count}\\\""`;
+      });
 
-      var prefix = nameModule;
-
-      var paramsToTpl = params.join(', ')
-var tpl = `
-  "is${nameModule}": {
-    "prefix": "${prefix}",
-    "body": ["<is${nameModule}", ${paramsToTpl}, "/>"],
-    "description": "is${nameModule}" 
-  }`;
+      paramsToTpl = params.join(', ')
+      tpl = `
+        "is${nameModule}": {
+          "prefix": "${prefix}",
+          "body": ["<is${nameModule}", ${paramsToTpl}, "/>"],
+          "description": "is${nameModule}" 
+        }`;
 
       return tpl;
     }
   }();
 
   this.templatesSnippets.sublimetext = function name() {
-
     return function (module) {
-      var nameModule = module.name;
-      var paramsMod = module.params;
-      var params = [];
+      let tpl;
+      let snippet;
+      let nameModule = module.name;
+      let paramsMod = module.params;
+      let params = [];
 
-      for (var y = 0; y < paramsMod.length; y++) {
-        params.push(paramsMod[y] + '="\\$\\{$' + (y + 1) + '\\}"')
-      }
-      var snippet = '<is' + nameModule + ' ' + params.join(' ') + '/>';
+      params = paramsMod.map((param, index) => {
+        return param + '="\\$\\{$' + (index + 1) + '\\}"'
+      });
 
-      var tpl = `
-    <snippet>
-      <content><![CDATA[
-        ${snippet}
-      ]]></content>
-      <tabTrigger>is${nameModule}</tabTrigger>
-    </snippet>`;
+      snippet = '<is' + nameModule + ' ' + params.join(' ') + '/>';
+
+      tpl = `
+        <snippet>
+          <content><![CDATA[
+            ${snippet}
+          ]]></content>
+          <tabTrigger>is${nameModule}</tabTrigger>
+        </snippet>`;
 
       return tpl;
     }
   }();
 };
 
-GeneratorSnippets.prototype.prepareData = function () {
-  var regexQutes = /"/g;
-  var strNoWhiteSpace = this.stringIn.replace(/[\s\r\n]/gmi, '');
-  var regex = /(name="[a-z]+"[attribute="a-z-_\d]+)/gmi;
-  var modules = strNoWhiteSpace.match(regex);
+GeneratorSnippets.prototype.prepareWritter = function () {
+  function vsCodeWritter() {
+    this.writeSnippetFile(this.outputpath, '{\n' + this.generateAllSnippets().join(',\n') + '\n}' + '\n');
+  }
 
-  for (var i = 0; i < modules.length; i++) {
-    var currentModule = {
+  function sublimeTextWritter() {
+    let pathbase = this.outputpath;
+
+    this.modules.forEach(module => {
+      let pathOutputFile = pathbase + 'is' + module.name + '.sublime-snippet';
+      this.writeSnippetFile(pathOutputFile, this.generateSnippet(module));
+    });
+  }
+
+  this.writters = {
+    vscode: vsCodeWritter,
+    sublimetext: sublimeTextWritter
+  };
+
+  // if the writter exist if not vscode is defautl
+  this.writter = this.writters[this.editorTarget] ? this.writters[this.editorTarget] : this.writters.vscode;
+};
+
+GeneratorSnippets.prototype.getData = function (modulesFilePath) {
+  let data = fs.readFileSync(modulesFilePath, 'utf8');
+
+  return data;
+};
+
+GeneratorSnippets.prototype.prepareData = function () {
+  let regexQutes = /"/g;
+  let strNoWhiteSpace = this.stringIn.replace(/[\s\r\n]/gmi, '');
+  let regex = /(name="[a-z]+"[attribute="a-z-_\d]+)/gmi;
+  let modules = strNoWhiteSpace.match(regex);
+
+  this.modules = modules.map(module => {
+    let infosModule = module.split('attribute')
+    let nameModule = infosModule[0].replace('name=', '').replace(regexQutes, '');
+    let currentModule = {
       name: '',
       params: []
     };
-    var infosModule = modules[i].split('attribute')
-    var nameModule = infosModule[0].replace('name=', '').replace(regexQutes, '');
+
     currentModule.name = nameModule;
+    currentModule.params = infosModule.map(infoMod => {
+      let nameArg = infoMod.replace('=', '').replace(regexQutes, '')
+      return nameArg;
+    });
 
-    for (var y = 1; y < infosModule.length; y++) {
-      var nameArg = infosModule[y].replace('=', '').replace(regexQutes, '')
-      currentModule.params.push(nameArg)
-    }
-    this.modules.push(currentModule)
-  }
-
+    return currentModule;
+  });
 };
 
 GeneratorSnippets.prototype.generateSnippet = function (module) {
-  var templateType = this.templatesSnippets[this.editorTarget] ? this.templatesSnippets[this.editorTarget] : () => { };
+  let templateType = this.templatesSnippets[this.editorTarget]
+    ? this.templatesSnippets[this.editorTarget]
+    :
+    module => {
+      console.log('Template undefined for ' + this.editorTarget, 'module name ' + module.name)
+    };
   return templateType(module)
 };
 
-GeneratorSnippets.prototype.generateAllSnippets = function (module) {
-  var output;
-
-  output = this.modules.map((curr) => this.generateSnippet(curr))
+GeneratorSnippets.prototype.generateAllSnippets = function () {
+  let output = [];
+  output = this.modules.map(curr => this.generateSnippet(curr))
 
   return output;
 };
 
+GeneratorSnippets.prototype.writeSnippets = function () {
+  this.writter();
+};
+
+/**
+ * write file snippets
+ * @param {pathSystem} targetPath 
+ * @param {string} snippetsString 
+ */
+GeneratorSnippets.prototype.writeSnippetFile = function (targetPath, snippetsString) {
+  fs.writeFile(targetPath, snippetsString, function (err) {
+    if (err) throw err;
+
+    let msgSaved = ' saved in next path ';
+    let targetPathLength = targetPath.length;
+    // ceil to avoid as decimal number
+    let nbNeeds = Math.ceil((targetPathLength - msgSaved.length) / 2);
+    let strSide = generateSuiteChars(nbNeeds + 1, '=');
+
+    console.log(strSide + msgSaved.toUpperCase() + strSide);
+    console.log(targetPath);
+    console.log(generateSuiteChars(targetPathLength + 1, '='));
+  });
+};
+
 module.exports = GeneratorSnippets;
+
+// ==============================================================
+// ============ HELPERS TO DISPLAY MESSAGE SAVED IN CONSOLE =====
+// ==============================================================
+
+/**
+ * helper to generate a string with repeted char
+ * @param {number} nb 
+ * @param {string} char 
+ */
+function generateSuiteChars(nb, char) {
+  try {
+    return (new Array(nb).join(char))
+  } catch (error) {
+    console.log(nb, char)
+    console.log(error);
+    return '================================================';
+  }
+}
